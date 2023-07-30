b5368cf7bce3a8ab4d938a8a666d9bf357de53e5
Gustavo Salgado <gsalgado@lwolf.com>
2023-07-30 12:26:16 -0500

    Fin seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi Primera  APP de Angular';
+
+  public counter : number = 10;
+
+  increaseBy(value: number):void{
+    this.counter +=1;
+ }
+
+ decriseBy(value: number):void{
+  this.counter -= 1;
+ }
+
+ resetValue():void{
+    this.counter = 0;
+ }
+
+
 }
